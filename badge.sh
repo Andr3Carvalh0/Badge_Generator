6aaaaf98abdd62ad577df87c93645f73d6d8d3e7
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-09-10 19:50:54 +0200

    Handle text position Add support for filename and background right color

--- badge.sh
+++ badge.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+CHAR_SIZE="68"
+FONT_SIZE="0.60"
+MARGIN="10"
+FONT_SCALE="100"
+
 help() {
 	echo ""
-	echo "Usage: $0 -k key -v value"
+	echo "Usage: $0 -k key -v value -c #hexValue"
 	echo "\t-k The value to be displayed on the left side of the badge"
 	echo "\t-v The value to be displayed on the right side of the badge"
+	echo "\t-c The hex value for the value side background. Must include #"
+	echo "\t-s The filename for the output file"
 	exit 1
 }
 
-while getopts "k:v:" opt; do
+while getopts "k:v:c:s:" opt; do
 	case "$opt" in
 	k) key="$OPTARG" ;;
 	v) value="$OPTARG" ;;
+	c) color="$OPTARG" ;;
+	s) filename="$OPTARG" ;;
 	?) help ;;
 	esac
 done
@@ -21,16 +30,19 @@ if [ -z "$key" ] || [ -z "$value" ]; then
 	help
 fi
 
-WIDTH_PER_LETTER="4.566"
-CONTAINER_RATIO="0.531"
-TEXT_FACTOR="100"
-MARGIN="10"
+if [ -z "$color" ]; then
+	color="#007ec6"
+fi
+
+if [ -z "$filename" ]; then
+	filename="badge"
+fi
 
 function calculateContainer() {
 	local text=$1
 	local amountOfLetters=${#text}
 
-	local width=$(awk -v letters="${amountOfLetters}" -v font_size="${WIDTH_PER_LETTER}" -v padding="${MARGIN}" 'BEGIN{result=(letters * font_size + (padding * 2)); print result;}')
+	local width=$(awk -v size="${amountOfLetters}" -v font_size="${CHAR_SIZE}" -v margin="${MARGIN}" 'BEGIN{result=(((size * font_size) / margin) + margin); print result;}')
 	
 	echo $width
 }
@@ -39,7 +51,7 @@ function calculateTextSize() {
 	local text=$1
 	local amountOfLetters=${#text}
 
-	local size=$(awk -v letters="${amountOfLetters}" -v font_size="${WIDTH_PER_LETTER}" -v padding="${MARGIN}" 'BEGIN{result=(letters * font_size + (padding * 2)); print result;}')
+	local size=$(awk -v size="${amountOfLetters}" -v font_size="${FONT_SIZE}" -v scale="${FONT_SCALE}" -v margin="${MARGIN}" 'BEGIN{result=(size * font_size * (scale + margin)); print result;}')
 	
 	echo $size
 }
@@ -48,45 +60,44 @@ function generate() {
 	local key=$1
 	local value=$2
 	
-	local leftContainerSize="$(calculateContainer $key)"
-	local rightContainerSize="$(calculateContainer $value)"
-	local totalSize=$(awk -v left="${leftContainerSize}" -v right="${rightContainerSize}" 'BEGIN{result=(left + right); print result;}')
+	local leftWidth="$(calculateContainer $key)"
+	local leftX=$(awk -v left="${leftWidth}" 'BEGIN{result=(((left / 2) + 1) * 10); print result;}')
+	local leftLength=$(calculateTextSize $key)
 
-	local leftX="261.5"
-	local leftLength="403.0"
+	local rightWidth="$(calculateContainer $value)"
+	local rightX=$(awk -v left="${leftWidth}" -v right="${rightWidth}" 'BEGIN{result=(((left + (right / 2)) - 1) * 10); print result;}')
+	local rightLength=$(calculateTextSize $value)
 
-	local rightX="715.5"
-	local rightLength="345.0"
+	local totalWidth=$(awk -v left="${leftWidth}" -v right="${rightWidth}" 'BEGIN{result=(left + right); print result;}')
 
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-			<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"${totalSize}\" height=\"20\">
+			<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"${totalWidth}\" height=\"20\">
 				<linearGradient id=\"smooth\" x2=\"0\" y2=\"100%\">
 					<stop offset=\"0\" stop-color=\"#bbb\" stop-opacity=\".1\" />
 					<stop offset=\"1\" stop-opacity=\".1\" />
 				</linearGradient>
 				<clipPath id=\"round\">
-					<rect width=\"${totalSize}\" height=\"20\" rx=\"3\" fill=\"#fff\" />
+					<rect width=\"${totalWidth}\" height=\"20\" rx=\"3\" fill=\"#fff\" />
 				</clipPath>
 				<g clip-path=\"url(#round)\">
-					<rect width=\"${leftContainerSize}\" height=\"20\" fill=\"#555\" />
-					<rect x=\"${leftContainerSize}\" width=\"${rightContainerSize}\" height=\"20\" fill=\"#007ec6\" />
-					<rect width=\"${totalSize}\" height=\"20\" fill=\"url(#smooth)\" />
+					<rect width=\"${leftWidth}\" height=\"20\" fill=\"#555\" />
+					<rect x=\"${leftWidth}\" width=\"${rightWidth}\" height=\"20\" fill=\"$color\" />
+					<rect width=\"${totalWidth}\" height=\"20\" fill=\"url(#smooth)\" />
 				</g>
 				<g fill=\"#fff\" text-anchor=\"middle\" font-family=\"DejaVu Sans,Verdana,Geneva,sans-serif\" font-size=\"110\">
 					<text x=\"${leftX}\" y=\"150\" fill=\"#010101\" fill-opacity=\"0.3\" transform=\"scale(0.1)\" textLength=\"${leftLength}\" lengthAdjust=\"spacing\">${key}</text>
-					<text x=\"${leftX}\" y=\"140\" transform=\"scale(0.1)\" textLength=\"403.0\" lengthAdjust=\"spacing\">${key}</text>
+					<text x=\"${leftX}\" y=\"140\" transform=\"scale(0.1)\" textLength=\"${leftLength}\" lengthAdjust=\"spacing\">${key}</text>
 					<text x=\"${rightX}\" y=\"150\" fill=\"#010101\" fill-opacity=\"0.3\" transform=\"scale(0.1)\" textLength=\"${rightLength}\" lengthAdjust=\"spacing\">${value}</text>
-					<text x=\"${rightX}\" y=\"140\" transform=\"scale(0.1)\" textLength=\"345.0\" lengthAdjust=\"spacing\">${value}</text>
+					<text x=\"${rightX}\" y=\"140\" transform=\"scale(0.1)\" textLength=\"${rightLength}\" lengthAdjust=\"spacing\">${value}</text>
 				</g>
 			</svg>"
 }
 
 function save() {
    local badge=$1
+   local filename=$2
 
-   echo $badge > badge.svg
+   echo $badge > ${filename}.svg
 }
 
-save "$(generate $key $value)"
-
-echo "Done!"
+save "$(generate $key $value)" $filename
